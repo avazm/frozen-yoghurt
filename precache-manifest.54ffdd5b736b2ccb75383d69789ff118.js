c68a8cd96efb266f69c51628b5d59883f7fb6785
Albert Vazquez <avazquezm@iMac-de-Albert.local>
2019-06-23 17:12:03 +0200

    Updates

---
+++ precache-manifest.54ffdd5b736b2ccb75383d69789ff118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30af8802ca27afcf1d4815fd27c93fe6",
+    "url": "/frozen-yoghurt/index.html"
+  },
+  {
+    "revision": "f4874d198caf8531fd88",
+    "url": "/frozen-yoghurt/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "6894c854f90b5b6b9649",
+    "url": "/frozen-yoghurt/static/css/main.13ecea59.chunk.css"
+  },
+  {
+    "revision": "f4874d198caf8531fd88",
+    "url": "/frozen-yoghurt/static/js/2.06fc48ab.chunk.js"
+  },
+  {
+    "revision": "6894c854f90b5b6b9649",
+    "url": "/frozen-yoghurt/static/js/main.f4a092f8.chunk.js"
+  },
+  {
+    "revision": "e4828633c21190ca52f7",
+    "url": "/frozen-yoghurt/static/js/runtime~main.65cab081.js"
+  }
+]);
